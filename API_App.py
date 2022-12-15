6e332194ba2974488cd45b8bfacb988aeb666cd3
shankarmaheswaran <49570293+shankarmaheswaran@users.noreply.github.com>
2022-12-15 20:07:25 +0530

    Add files via upload

---
+++ API_App.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+company_list = ['Oracle','Google','Microsoft','Amazon','Deloitte','123','abc']
+app = FastAPI()
+
+@app.get("/replace-string")
+def replace_words(company=None):
+
+    if company in company_list and '©️' not in company:
+        new_name = company+'©️'
+
+    elif company in company_list and '©️' in company:
+        new_name = company
+    else:
+        new_name = 'Entered name is not in the company list'
+
+
+    return new_name
