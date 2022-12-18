f10e8da47a18d24b88a385d210165d0ed09478e4
shankarmaheswaran <49570293+shankarmaheswaran@users.noreply.github.com>
2022-12-18 11:35:46 +0530

    Update API_App.py

--- Projects/API_App.py
+++ Projects/API_App.py
@@ -6,12 +6,13 @@ app = FastAPI()
 def replace_words(company=None):
 
     if company in company_list and '©️' not in company:
-        new_name = company+'©️'
+        new_name = 'We really like the new security features of '+company+'©️ Cloud'
 
     elif company in company_list and '©️' in company:
-        new_name = company
+        new_name = 'We really like the new security features of '+company+' Cloud'
     else:
         new_name = 'Entered name is not in the company list'
 
 
     return new_name
+
